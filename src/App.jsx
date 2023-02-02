01f8c40425d144f7b59ff89e9a8aee48ac7f71ee
edzy16 <edwinsiby16@gmail.com>
2023-02-02 19:35:56 +0530

    Basic Todo app with CRUD functionality completed

--- src/App.jsx
+++ src/App.jsx
@@ -15,19 +15,58 @@ function App() {
       </div>
       <div className="input">
         <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={() => setToDos([...toDos, { text: toDo, status: false }])} className="fas fa-plus"></i>
+        <i onClick={() => setToDos([...toDos, {id:Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        {toDos.map((value)=>{
-          return(<div className="todo">
-            <div className="left">
-              <input value={value.status} type="checkbox" name="" id="" />
-              <p>{value.text}</p>
+        {toDos.map((obj)=>{
+          return (
+            <div className="todo">
+              <div className="left">
+                <input
+                  onChange={(e) => {
+                    console.log(e.target.checked);
+                    console.log(obj);
+                    setToDos(
+                      toDos.filter((obj2) => {
+                        if (obj2.id === obj.id) {
+                          obj2.status = e.target.checked;
+                        }
+                        return obj2;
+                      })
+                    );
+                  }}
+                  value={obj.status}
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
+                <p>{obj.text}</p>
+              </div>
+              <div className="right">
+                <i
+                  id={obj.id}
+                  className="fas fa-times"
+                  onClick={(e) => {
+                    let index = toDos.findIndex((obj) => {
+                      return obj.id == e.target.id;
+                    });
+                    if (index !== -1) {
+                      toDos.splice(index, 1);
+                      setToDos([...toDos]);
+                    }
+                  }}
+                ></i>
+              </div>
             </div>
-            <div className="right">
-              <i className="fas fa-times"></i>
-            </div>
-          </div>)
+          );
+        })}
+        {toDos.map((obj) => {
+          if (obj.status) {
+            return (
+              <h1>{obj.text}</h1>
+            )
+          }
+          return null
         })}
       </div>
     </div>
